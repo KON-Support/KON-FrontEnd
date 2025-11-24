cdcbaa17232913a62fb0e8e062e158609878fcd6
rafadevgg <rafaelpc5671@gmail.com>
2025-11-24 17:13:36 -0300

    fix: Conflitos

--- src/app/views/user-dashboard/user-dashboard.ts
+++ src/app/views/user-dashboard/user-dashboard.ts
@@ -1,10 +1,6 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { TicketsUser } from '../../components/chamados-user/tickets-user';
-=======
 import { ChamadosUser } from '../../components/chamados-user/chamados-user';
 import { NavbarUsuario } from "../../components/navbar-usuario/navbar-usuario";
->>>>>>> 9ac9d23ff4b9298ec3fa7d394b093e67834bac35
 
 @Component({
   selector: 'app-user-dashboard',
