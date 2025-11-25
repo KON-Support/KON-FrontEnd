10cc2f267db4063f98ca0ac4b8271dfb1536da01
rafadevgg <rafaelpc5671@gmail.com>
2025-11-25 13:35:18 -0300

    fix: Resolvendo erros dashboard agente/admin

--- src/app/views/agente-dashboard/agente-dashboard.ts
+++ src/app/views/agente-dashboard/agente-dashboard.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { CardEstatistica } from '../../components/card-estatistica/card-estatistica';
 import { ChamadosNaoAtribuidos } from '../../components/chamados-nao-atribuidos/chamados-nao-atribuidos';
-import { NavbarAdmin } from "../../components/navbar-admin/navbar-admin";
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-agente-dashboard',
   standalone: true,
-  imports: [CardEstatistica, ChamadosNaoAtribuidos, NavbarAdmin],
+  imports: [CardEstatistica, ChamadosNaoAtribuidos, Navbar],
   templateUrl: './agente-dashboard.html',
   styleUrl: './agente-dashboard.scss',
 })
