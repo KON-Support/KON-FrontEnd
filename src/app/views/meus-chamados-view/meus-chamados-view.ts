321d9e793d7969e7068519dfde34bf2b3fda348f
Luiz Henrique <lhmendes0911@gmail.com>
2025-11-25 17:23:33 -0300

    fix: arrumando imports

--- src/app/views/meus-chamados-view/meus-chamados-view.ts
+++ src/app/views/meus-chamados-view/meus-chamados-view.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { ChamadosUser } from "../../components/chamados-user/chamados-user";
-import { NavbarUsuario } from "../../components/navbar-usuario/navbar-usuario";
+import { Navbar } from '../../components/navbar/navbar';
 
 @Component({
   selector: 'app-meus-chamados-view',
-  imports: [ChamadosUser, NavbarUsuario],
+  imports: [ChamadosUser, Navbar],
   templateUrl: './meus-chamados-view.html',
   styleUrl: './meus-chamados-view.scss',
 })
